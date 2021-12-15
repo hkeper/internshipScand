4a2e30c68711ae7bd0f6634f74851d3e7b5232e6
Mikhail Makeikin <hkeper1@gmail.com>
2021-12-15 20:19:04 +0300

    First commit: add Fragment for Map Editor; add dependencies for navigation

---
+++ app/src/main/java/com/scand/internship/mars_scout/MapEditorFragment.kt
@@ -0,0 +1,27 @@
+package com.scand.internship.mars_scout
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MapEditorFragment : Fragment() {
+
+    private lateinit var viewModel: MapEditorViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.map_editor_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MapEditorViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/scand/internship/mars_scout/MapEditorViewModel.kt
@@ -0,0 +1,7 @@
+package com.scand.internship.mars_scout
+
+import androidx.lifecycle.ViewModel
+
+class MapEditorViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
